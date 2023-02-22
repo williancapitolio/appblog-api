85fee6a6d900491d10aecee377611fe2090ac180
Willian <williansilvap25@gmail.com>
2023-02-22 01:58:07 -0300

    now i can see cookie on browser

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { upload, uploadImgMiddleware } from "./config/multer.js";
 const port = process.env.PORT || 8800;
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.post("/api/upload", upload, uploadImgMiddleware);
