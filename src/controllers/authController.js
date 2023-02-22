f877fd79e6345f8246e2b5be39ec56bb3f1f76cf
Willian <williansilvap25@gmail.com>
2023-02-21 23:32:54 -0300

    translate responde login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -34,10 +34,10 @@ export const login = (req, res) => {
         const q = "SELECT * FROM users WHERE username = ?"
         db.query(q, [req.body.username], (err, data) => {
             if (err) return res.status(400).json(err);
-            if (data.length === 0) return res.status(404).json("User not found!");
+            if (data.length === 0) return res.status(404).json("Usuário não encontrado!");
             //CHECK PASSWORD
             const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
-            if (!isPasswordCorrect) return res.status(400).json("Wrong username or password");
+            if (!isPasswordCorrect) return res.status(400).json("Usuário ou senha incorreto!");
             const token = jwt.sign({ id: data[0].id }, process.env.JWT_SECRET);
             const { password, ...other } = data[0];
             res.cookie("access_token", token, { httpOnly: true }).status(200).json(other);
